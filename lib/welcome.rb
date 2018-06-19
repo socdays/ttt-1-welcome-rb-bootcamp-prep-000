c83ef3c34be92a55a6b81ac4def1b1bfe9117d18
Socorro Diaz <sodays1@gmail.com>
2018-06-19 00:40:11 +0000

    automatically backed up by learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -5,12 +5,12 @@ puts "Welcom to Tic Tac Toe!"
 puts "Welcome to Tic Tac Toe!"
 print "Welcome to Tic Tac Toe!"
 run test "something interesting" do
-  
+
 end
 print
 puts
 run test "something interesting" do
-  
+
 end
 puts "Welcome to Tic Tac Toe!"
 prints "Welcome to Tic Tac Toe!"
